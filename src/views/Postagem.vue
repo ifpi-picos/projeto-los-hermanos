dd5a40e1acd055e48fabe82489c6905fc59b180d
Samuel Martins <samuel.ubuntu@gmail.com>
2021-01-07 09:09:44 -0300

    Salvando informaçoes das postagens

--- src/views/Postagem.vue
+++ src/views/Postagem.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
 
-    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    <form class="post">
+      <b-row>
+        <b-col md="12">
+          <b-form-group 
+          label="Título da postagem"
+          label-for="postTitle">
+          <b-form-input id="postTitle"> {{postTitle}} </b-form-input>
+
+        </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="12">
+          <b-form-group class="editor">
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+      </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md=4>
+          <b-button @click="savePost" variant="info">Salvar</b-button>
+        </b-col>
+      </b-row>
+      
+    </form>
+
+    
     <!-- <div id="editor">
       <h3 class="text-center">Fazer uma Nova Postagem</h3>
       <form>
@@ -39,28 +65,42 @@ export default {
     return {
       editor: ClassicEditor,
         editorData: '',
+        postTitle: '',
         editorConfig: {}
     }
   },
 
   methods: {
-    salvar () {
-      this.$firebase
-        .firestore()
-        .collection('posts')
-        .add(this.post)
-        .then(function (docRef) {
-          console.log('Document written with ID: ', docRef.id)
-        })
-        .catch(function (error) {
-          console.error('Error adding document: ', error)
-        })
+    savePost () {
+      const postData = {
+        title: this.postTitle,
+        content: this.editorData,
+        createdAt: new Date(),
+        createdBy: this.$firebase.auth().currentUser.displayName,
+        createdByUid: this.$firebase.auth().currentUser.uid
+      }
+
+      this.$firebase.firestore().collection('posts').add(postData).then(() => {
+        this.$router.push('/posts');
+      })
+      // this.$firebase
+      //   .firestore()
+      //   .collection('posts')
+      //   .add(this.post)
+      //   .then(function (docRef) {
+      //     console.log('Document written with ID: ', docRef.id)
+      //   })
+      //   .catch(function (error) {
+      //     console.error('Error adding document: ', error)
+      //   })
     }
   }
 }
 </script>
 
 <style>
-
+.post {
+  margin: 0 2%;
+}
 
 </style>
